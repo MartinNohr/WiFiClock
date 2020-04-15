0ea7d3fa6f1a6cb31a1fbb44c9aeade1620d748a
Martin Nohr <martinnohr76@gmail.com>
2020-04-15 07:44:51 -0600

    Added reading the SD card code.

--- WiFiClock.ino
+++ WiFiClock.ino
@@ -7,11 +7,18 @@
 #include "heltec.h"
 #include "WiFi.h"
 #include "images.h"
+#include "FS.h"
+#include "SD.h"
+#include "SPI.h"
 
+// SD details
+#define SDcsPin 5                        // SD card CS pin
+SPIClass spi1;
+
+
+// time details
 const long utcOffsetInSeconds = -6 * 3600;
-
 char daysOfTheWeek[7][12] = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
-
 // Define NTP Client to get time
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds, 3600);
@@ -133,12 +140,181 @@ void setup()
 	Serial.println(WiFi.localIP());
 	timeClient.begin();
 	Heltec.display->clear();
+	setupSDcard();
+	pinMode(17, OUTPUT);
+}
+
+void listDir(fs::FS& fs, const char* dirname, uint8_t levels) {
+	Serial.printf("Listing directory: %s\n", dirname);
+
+	File root = fs.open(dirname);
+	if (!root) {
+		Serial.println("Failed to open directory");
+		return;
+	}
+	if (!root.isDirectory()) {
+		Serial.println("Not a directory");
+		return;
+	}
+
+	File file = root.openNextFile();
+	while (file) {
+		if (file.isDirectory()) {
+			Serial.print("  DIR : ");
+			Serial.print(file.name());
+			time_t t = file.getLastWrite();
+			struct tm* tmstruct = localtime(&t);
+			Serial.printf("  LAST WRITE: %d-%02d-%02d %02d:%02d:%02d\n", (tmstruct->tm_year) + 1900, (tmstruct->tm_mon) + 1, tmstruct->tm_mday, tmstruct->tm_hour, tmstruct->tm_min, tmstruct->tm_sec);
+			if (levels) {
+				listDir(fs, file.name(), levels - 1);
+			}
+		}
+		else {
+			Serial.print("  FILE: ");
+			Serial.print(file.name());
+			Serial.print("  SIZE: ");
+			Serial.print(file.size());
+			time_t t = file.getLastWrite();
+			struct tm* tmstruct = localtime(&t);
+			Serial.printf("  LAST WRITE: %d-%02d-%02d %02d:%02d:%02d\n", (tmstruct->tm_year) + 1900, (tmstruct->tm_mon) + 1, tmstruct->tm_mday, tmstruct->tm_hour, tmstruct->tm_min, tmstruct->tm_sec);
+		}
+		file = root.openNextFile();
+	}
+}
+
+
+void createDir(fs::FS& fs, const char* path) {
+	Serial.printf("Creating Dir: %s\n", path);
+	if (fs.mkdir(path)) {
+		Serial.println("Dir created");
+	}
+	else {
+		Serial.println("mkdir failed");
+	}
+}
+
+void removeDir(fs::FS& fs, const char* path) {
+	Serial.printf("Removing Dir: %s\n", path);
+	if (fs.rmdir(path)) {
+		Serial.println("Dir removed");
+	}
+	else {
+		Serial.println("rmdir failed");
+	}
+}
+
+void readFile(fs::FS& fs, const char* path) {
+	Serial.printf("Reading file: %s\n", path);
+
+	File file = fs.open(path);
+	if (!file) {
+		Serial.println("Failed to open file for reading");
+		return;
+	}
+
+	Serial.print("Read from file: ");
+	while (file.available()) {
+		Serial.write(file.read());
+	}
+	file.close();
+}
+
+void writeFile(fs::FS& fs, const char* path, const char* message) {
+	Serial.printf("Writing file: %s\n", path);
+
+	File file = fs.open(path, FILE_WRITE);
+	if (!file) {
+		Serial.println("Failed to open file for writing");
+		return;
+	}
+	if (file.print(message)) {
+		Serial.println("File written");
+	}
+	else {
+		Serial.println("Write failed");
+	}
+	file.close();
+}
+
+void appendFile(fs::FS& fs, const char* path, const char* message) {
+	Serial.printf("Appending to file: %s\n", path);
+
+	File file = fs.open(path, FILE_APPEND);
+	if (!file) {
+		Serial.println("Failed to open file for appending");
+		return;
+	}
+	if (file.print(message)) {
+		Serial.println("Message appended");
+	}
+	else {
+		Serial.println("Append failed");
+	}
+	file.close();
+}
+
+void renameFile(fs::FS& fs, const char* path1, const char* path2) {
+	Serial.printf("Renaming file %s to %s\n", path1, path2);
+	if (fs.rename(path1, path2)) {
+		Serial.println("File renamed");
+	}
+	else {
+		Serial.println("Rename failed");
+	}
+}
+
+void deleteFile(fs::FS& fs, const char* path) {
+	Serial.printf("Deleting file: %s\n", path);
+	if (fs.remove(path)) {
+		Serial.println("File deleted");
+	}
+	else {
+		Serial.println("Delete failed");
+	}
+}
+
+void setupSDcard() {
+	pinMode(SDcsPin, OUTPUT);
+	SPIClass(1);
+	spi1.begin(18, 19, 23, SDcsPin);	// SCK,MISO,MOSI,CS
+
+	if (!SD.begin(SDcsPin, spi1)) {
+		Serial.println("Card Mount Failed");
+		return;
+	}
+	uint8_t cardType = SD.cardType();
+
+	if (cardType == CARD_NONE) {
+		Serial.println("No SD card attached");
+		return;
+	}
+
+	Serial.print("SD Card Type: ");
+	if (cardType == CARD_MMC) {
+		Serial.println("MMC");
+	}
+	else if (cardType == CARD_SD) {
+		Serial.println("SDSC");
+	}
+	else if (cardType == CARD_SDHC) {
+		Serial.println("SDHC");
+	}
+	else {
+		Serial.println("UNKNOWN");
+	}
+
+	uint64_t cardSize = SD.cardSize() / (1024 * 1024);
+	Serial.printf("SD Card Size: %lluMB\n", cardSize);
+
+	listDir(SD, "/", 0);
+	//GetFileNamesFromSD(currentFolder);
 }
 
 int timeZone = -6;
 #define SECS_PER_HOUR 3600
 void loop()
 {
+	static int lastSec;
     timeClient.update();
     time_t timenow = timeClient.getEpochTime();
 	struct tm* gtime;
@@ -152,24 +328,30 @@ void loop()
 	//delay(1000);
     //int temp = temperatureRead();
     //Serial.println("temp:" + String(temp));
-	Heltec.display->clear();
-	Heltec.display->setFont(ArialMT_Plain_24);
-	char line[50];
-	int hour = gtime->tm_hour;
-	const char* ampm = (hour < 13 ? "AM" : "PM");
-	hour = hour % 12;
-	if (hour == 0)
-		hour = 12;
-	sprintf(line, "%2d:%02d:%02d", hour, gtime->tm_min, gtime->tm_sec);
-	Heltec.display->drawString(0, 0, line);
-	Heltec.display->setFont(ArialMT_Plain_10);
-	Heltec.display->drawString(100, 0, ampm);
-	Heltec.display->setFont(ArialMT_Plain_16);
-    sprintf(line, "%d/%d/%4d", gtime->tm_mon + 1, gtime->tm_mday, gtime->tm_year + 1900);
-	Heltec.display->drawString(0, 25, line);
-	Heltec.display->setFont(ArialMT_Plain_10);
-	Heltec.display->drawString(0, 45, daysOfTheWeek[gtime->tm_wday]);
-	Heltec.display->display();
-    //Serial.println(timeClient.getFormattedTime());
-	delay(1000);
+	if (lastSec != gtime->tm_sec) {
+		lastSec = gtime->tm_sec;
+		Heltec.display->clear();
+		Heltec.display->setFont(ArialMT_Plain_24);
+		char line[50];
+		int hour = gtime->tm_hour;
+		const char* ampm = (hour < 13 ? "AM" : "PM");
+		hour = hour % 12;
+		if (hour == 0)
+			hour = 12;
+		sprintf(line, "%2d:%02d:%02d", hour, gtime->tm_min, gtime->tm_sec);
+		Heltec.display->drawString(0, 0, line);
+		Heltec.display->setFont(ArialMT_Plain_10);
+		Heltec.display->drawString(100, 0, ampm);
+		Heltec.display->setFont(ArialMT_Plain_16);
+		sprintf(line, "%d/%d/%4d", gtime->tm_mon + 1, gtime->tm_mday, gtime->tm_year + 1900);
+		Heltec.display->drawString(0, 25, line);
+		Heltec.display->setFont(ArialMT_Plain_10);
+		Heltec.display->drawString(0, 45, daysOfTheWeek[gtime->tm_wday]);
+		Heltec.display->display();
+		//Serial.println(timeClient.getFormattedTime());
+	}
+	digitalWrite(17, HIGH);
+	delay(5);
+	digitalWrite(17, LOW);
+	delay(10);
 }
