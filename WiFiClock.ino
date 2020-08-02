a8a66bcad0a50662061f51a50f121ead6a7f402d
Martin Nohr <martin.nohr@nohrphoto.com>
2020-08-02 09:07:51 -0600

    Updated lots of code. Added rotary switch handler.

--- WiFiClock.ino
+++ WiFiClock.ino
@@ -6,15 +6,8 @@
 
 #include "heltec.h"
 #include "WiFi.h"
-#include "images.h"
-#include "FS.h"
-#include "SD.h"
-#include "SPI.h"
 
-// SD details
-#define SDcsPin 5                        // SD card CS pin
-SPIClass spi1;
-
+#define OLED Heltec.display
 
 // time details
 const long utcOffsetInSeconds = -6 * 3600;
@@ -23,10 +16,90 @@ char daysOfTheWeek[7][12] = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thurs
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds, 3600);
 
-void logo() {
-	Heltec.display->clear();
-	Heltec.display->drawXbm(0, 5, logo_width, logo_height, (const unsigned char*)logo_bits);
-	Heltec.display->display();
+#include "DHT.h"
+
+#include <ThingerESP32.h>
+
+#define DHTPIN 14
+// rotary switch
+#define BTNPUSH 27
+#define BTNA 33
+#define BTNB 32
+
+#define LED 2
+
+int buttons;
+DHT dht(DHTPIN, DHT22);
+ThingerESP32 thing("MartinNohr", "TempHum", "zHIyT&vMRt!d");
+// interrupt handlers
+void IRAM_ATTR IntBtnCenter()
+{
+	static unsigned long pressedTime = 0;
+	noInterrupts();
+	bool val = digitalRead(BTNPUSH);
+	unsigned long currentTime = millis();
+	if (currentTime > pressedTime + 50) {
+		if (val == true) {
+			if (currentTime > pressedTime + 750) {
+				Serial.println("long press");
+			}
+			else {
+				Serial.println("high");
+			}
+		}
+		else {
+			Serial.println("low");
+		}
+		// got one, note time so we can ignore until ready again
+		pressedTime = currentTime;
+	}
+	interrupts();
+}
+
+// state table for the rotary encoder
+#define T true
+#define F false
+#define DIRECTIONS 2
+#define MAXSTATE 4
+#define CONTACTS 2
+bool stateTest[DIRECTIONS][MAXSTATE][CONTACTS] =
+{
+	{{T,F},{F,F},{F,T},{F,F}},
+	{{T,F},{F,F},{F,T},{F,F}}
+};
+#define A 0
+#define B 1
+void IRAM_ATTR IntBtnAB()
+{
+	static bool forward;
+	static int state = 0;
+	noInterrupts();
+	bool valA = digitalRead(BTNA);
+	bool valB = digitalRead(BTNB);
+	if (state == 0) {
+		// starting
+		// see if one of the first tests is correct, then go to state 1
+		if (stateTest[0][state][A] == valA && stateTest[0][state][B] == valB) {
+			forward = true;
+			++state;
+		}
+		else if (stateTest[0][state][A] == valB && stateTest[0][state][B] == valA) {
+			forward = false;
+			++state;
+		}
+	}
+	else {
+		// check if we can advance
+		if (stateTest[forward ? 1 : 0][state][A] == valA && stateTest[0][state][B] == valB) {
+			++state;
+		}
+	}
+	if (state == MAXSTATE) {
+		// we're done
+		Serial.println(String("rotary: ") + forward ? "forward" : "reverse");
+		state = 0;
+	}
+	interrupts();
 }
 
 void WIFISetUp(void)
@@ -40,43 +113,44 @@ void WIFISetUp(void)
 	delay(100);
 
 	byte count = 0;
+	OLED->clear();
 	while (WiFi.status() != WL_CONNECTED && count < 10)
 	{
 		count++;
 		delay(500);
-		Heltec.display->drawString(0, 0, "Connecting...");
-		Heltec.display->display();
+		OLED->drawString(0, 0, "Connecting...");
+		OLED->display();
 	}
 
-	Heltec.display->clear();
+	OLED->clear();
 	if (WiFi.status() == WL_CONNECTED)
 	{
-		Heltec.display->drawString(0, 0, "Connecting...OK.");
-		Heltec.display->display();
+		OLED->drawString(0, 0, "Connecting...OK.");
+		OLED->display();
 		//		delay(500);
 	}
 	else
 	{
-		Heltec.display->clear();
-		Heltec.display->drawString(0, 0, "Connecting...Failed");
-		Heltec.display->display();
+		OLED->clear();
+		OLED->drawString(0, 0, "Connecting...Failed");
+		OLED->display();
 		while (1);
 	}
-	Heltec.display->drawString(0, 10, "WIFI Setup done");
-	Heltec.display->display();
+	OLED->drawString(0, 10, "WIFI Setup done");
+	OLED->display();
 	delay(500);
 }
 
 void WIFIScan(void)
 {
-	Heltec.display->drawString(0, 20, "Scan start...");
-	Heltec.display->display();
+	OLED->drawString(0, 20, "Scan start...");
+	OLED->display();
 
 	int n = WiFi.scanNetworks();
-	Heltec.display->drawString(0, 30, "Scan done");
-	Heltec.display->display();
+	OLED->drawString(0, 30, "Scan done");
+	OLED->display();
 	delay(500);
-	Heltec.display->clear();
+	OLED->clear();
 	// find the ones with strength above -90
 	for (int ix = 0; ix < n; ++ix) {
 		if (WiFi.RSSI(ix) < -90) {
@@ -87,50 +161,59 @@ void WIFIScan(void)
 
 	if (n == 0)
 	{
-		Heltec.display->clear();
-		Heltec.display->drawString(0, 0, "no network found");
-		Heltec.display->display();
+		OLED->clear();
+		OLED->drawString(0, 0, "no network found");
+		OLED->display();
 		while (1);
 	}
 	else
 	{
-		Heltec.display->drawString(0, 0, (String)n);
-		Heltec.display->drawString(14, 0, "networks found:");
-		Heltec.display->display();
+		OLED->drawString(0, 0, (String)n);
+		OLED->drawString(14, 0, "networks found:");
+		OLED->display();
 		delay(500);
 
 		for (int i = 0; i < n; ++i) {
 			if (i && ((i / 6) * 6 == i)) {
 				delay(3000);
-				Heltec.display->clear();
+				OLED->clear();
 			}
 			// Print SSID and RSSI for each network found
-            Heltec.display->drawString(0, ((i % 6) + 1) * 9, (String)(i + 1));
-            Heltec.display->drawString(6, ((i % 6) + 1) * 9, ":");
-            Heltec.display->drawString(12, ((i % 6) + 1) * 9, (String)(WiFi.SSID(i)));
-            Heltec.display->drawString(90, ((i % 6) + 1) * 9, " (");
-            Heltec.display->drawString(98, ((i % 6) + 1) * 9, (String)(WiFi.RSSI(i)));
-            Heltec.display->drawString(114, ((i % 6) + 1) * 9, ")");
+            OLED->drawString(0, ((i % 6) + 1) * 9, (String)(i + 1));
+            OLED->drawString(6, ((i % 6) + 1) * 9, ":");
+            OLED->drawString(12, ((i % 6) + 1) * 9, (String)(WiFi.SSID(i)));
+            OLED->drawString(90, ((i % 6) + 1) * 9, " (");
+            OLED->drawString(98, ((i % 6) + 1) * 9, (String)(WiFi.RSSI(i)));
+            OLED->drawString(114, ((i % 6) + 1) * 9, ")");
 			//            display.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
-			Heltec.display->display();
+			OLED->display();
 			delay(10);
 		}
 	}
-	Heltec.display->display();
+	OLED->display();
 	delay(800);
-	Heltec.display->clear();
+	OLED->clear();
 }
 
 void setup()
 {
 	pinMode(LED, OUTPUT);
 	digitalWrite(LED, HIGH);
+	pinMode(BTNPUSH, INPUT_PULLUP);
+	pinMode(BTNA, INPUT_PULLUP);
+	pinMode(BTNB, INPUT_PULLUP);
+	pinMode(DHTPIN, INPUT_PULLUP);
+	attachInterrupt(digitalPinToInterrupt(BTNPUSH), IntBtnCenter, CHANGE);
+	attachInterrupt(digitalPinToInterrupt(BTNA), IntBtnAB, CHANGE);
+	attachInterrupt(digitalPinToInterrupt(BTNB), IntBtnAB, CHANGE);
+	dht.begin();
+	thing.add_wifi("NohrNet", "8017078120");
+	thing["dht22"] >> [](pson& out) {
+		out["humidity"] = dht.readHumidity();
+		out["temperature"] = dht.readTemperature(true);
+	};
 
 	Heltec.begin(true /*DisplayEnable Enable*/, false /*LoRa Enable*/, true /*Serial Enable*/);
-
-	logo();
-	delay(300);
-	Heltec.display->clear();
 
 	WIFISetUp();
 	//WIFIScan();
@@ -139,182 +222,14 @@ void setup()
 	Serial.println("IP address: ");
 	Serial.println(WiFi.localIP());
 	timeClient.begin();
-	Heltec.display->clear();
-	setupSDcard();
+	OLED->clear();
 	pinMode(17, OUTPUT);
-}
-
-void listDir(fs::FS& fs, const char* dirname, uint8_t levels) {
-	Serial.printf("Listing directory: %s\n", dirname);
-
-	File root = fs.open(dirname);
-	if (!root) {
-		Serial.println("Failed to open directory");
-		return;
-	}
-	if (!root.isDirectory()) {
-		Serial.println("Not a directory");
-		return;
-	}
-
-	File file = root.openNextFile();
-	while (file) {
-		if (file.isDirectory()) {
-			Serial.print("  DIR : ");
-			Serial.print(file.name());
-			time_t t = file.getLastWrite();
-			struct tm* tmstruct = localtime(&t);
-			Serial.printf("  LAST WRITE: %d-%02d-%02d %02d:%02d:%02d\n", (tmstruct->tm_year) + 1900, (tmstruct->tm_mon) + 1, tmstruct->tm_mday, tmstruct->tm_hour, tmstruct->tm_min, tmstruct->tm_sec);
-			if (levels) {
-				listDir(fs, file.name(), levels - 1);
-			}
-		}
-		else {
-			Serial.print("  FILE: ");
-			Serial.print(file.name());
-			Serial.print("  SIZE: ");
-			Serial.print(file.size());
-			time_t t = file.getLastWrite();
-			struct tm* tmstruct = localtime(&t);
-			Serial.printf("  LAST WRITE: %d-%02d-%02d %02d:%02d:%02d\n", (tmstruct->tm_year) + 1900, (tmstruct->tm_mon) + 1, tmstruct->tm_mday, tmstruct->tm_hour, tmstruct->tm_min, tmstruct->tm_sec);
-		}
-		file = root.openNextFile();
-	}
-}
-
-
-void createDir(fs::FS& fs, const char* path) {
-	Serial.printf("Creating Dir: %s\n", path);
-	if (fs.mkdir(path)) {
-		Serial.println("Dir created");
-	}
-	else {
-		Serial.println("mkdir failed");
-	}
-}
-
-void removeDir(fs::FS& fs, const char* path) {
-	Serial.printf("Removing Dir: %s\n", path);
-	if (fs.rmdir(path)) {
-		Serial.println("Dir removed");
-	}
-	else {
-		Serial.println("rmdir failed");
-	}
-}
-
-void readFile(fs::FS& fs, const char* path) {
-	Serial.printf("Reading file: %s\n", path);
-
-	File file = fs.open(path);
-	if (!file) {
-		Serial.println("Failed to open file for reading");
-		return;
-	}
-
-	Serial.print("Read from file: ");
-	while (file.available()) {
-		Serial.write(file.read());
-	}
-	file.close();
-}
-
-void writeFile(fs::FS& fs, const char* path, const char* message) {
-	Serial.printf("Writing file: %s\n", path);
-
-	File file = fs.open(path, FILE_WRITE);
-	if (!file) {
-		Serial.println("Failed to open file for writing");
-		return;
-	}
-	if (file.print(message)) {
-		Serial.println("File written");
-	}
-	else {
-		Serial.println("Write failed");
-	}
-	file.close();
-}
-
-void appendFile(fs::FS& fs, const char* path, const char* message) {
-	Serial.printf("Appending to file: %s\n", path);
-
-	File file = fs.open(path, FILE_APPEND);
-	if (!file) {
-		Serial.println("Failed to open file for appending");
-		return;
-	}
-	if (file.print(message)) {
-		Serial.println("Message appended");
-	}
-	else {
-		Serial.println("Append failed");
-	}
-	file.close();
-}
-
-void renameFile(fs::FS& fs, const char* path1, const char* path2) {
-	Serial.printf("Renaming file %s to %s\n", path1, path2);
-	if (fs.rename(path1, path2)) {
-		Serial.println("File renamed");
-	}
-	else {
-		Serial.println("Rename failed");
-	}
-}
-
-void deleteFile(fs::FS& fs, const char* path) {
-	Serial.printf("Deleting file: %s\n", path);
-	if (fs.remove(path)) {
-		Serial.println("File deleted");
-	}
-	else {
-		Serial.println("Delete failed");
-	}
-}
-
-void setupSDcard() {
-	pinMode(SDcsPin, OUTPUT);
-	SPIClass(1);
-	spi1.begin(18, 19, 23, SDcsPin);	// SCK,MISO,MOSI,CS
-
-	if (!SD.begin(SDcsPin, spi1)) {
-		Serial.println("Card Mount Failed");
-		return;
-	}
-	uint8_t cardType = SD.cardType();
-
-	if (cardType == CARD_NONE) {
-		Serial.println("No SD card attached");
-		return;
-	}
-
-	Serial.print("SD Card Type: ");
-	if (cardType == CARD_MMC) {
-		Serial.println("MMC");
-	}
-	else if (cardType == CARD_SD) {
-		Serial.println("SDSC");
-	}
-	else if (cardType == CARD_SDHC) {
-		Serial.println("SDHC");
-	}
-	else {
-		Serial.println("UNKNOWN");
-	}
-
-	uint64_t cardSize = SD.cardSize() / (1024 * 1024);
-	Serial.printf("SD Card Size: %lluMB\n", cardSize);
-
-	listDir(SD, "/", 0);
-	//GetFileNamesFromSD(currentFolder);
 }
 
 int timeZone = -6;
 #define SECS_PER_HOUR 3600
 void loop()
 {
-	static int lastSec;
     timeClient.update();
     time_t timenow = timeClient.getEpochTime();
 	struct tm* gtime;
@@ -328,31 +243,45 @@ void loop()
 	//delay(1000);
     //int temp = temperatureRead();
     //Serial.println("temp:" + String(temp));
-	if (lastSec != gtime->tm_sec) {
-		lastSec = gtime->tm_sec;
-		Heltec.display->clear();
-		Heltec.display->setFont(ArialMT_Plain_24);
-		char line[50];
-		int hour = gtime->tm_hour;
-		const char* ampm = (hour < 13 ? "AM" : "PM");
-		hour = hour % 12;
-		if (hour == 0)
-			hour = 12;
-		sprintf(line, "%2d:%02d:%02d", hour, gtime->tm_min, gtime->tm_sec);
-		Heltec.display->drawString(0, 0, line);
-		Heltec.display->setFont(ArialMT_Plain_10);
-		Heltec.display->drawString(100, 0, ampm);
-		Heltec.display->setFont(ArialMT_Plain_16);
-		sprintf(line, "%d/%d/%4d", gtime->tm_mon + 1, gtime->tm_mday, gtime->tm_year + 1900);
-		Heltec.display->drawString(0, 25, line);
-		Heltec.display->setFont(ArialMT_Plain_10);
-		Heltec.display->drawString(0, 45, daysOfTheWeek[gtime->tm_wday]);
-		Heltec.display->display();
-		//Serial.println(timeClient.getFormattedTime());
+	OLED->clear();
+	OLED->setFont(ArialMT_Plain_24);
+	char line[50];
+	int hour = gtime->tm_hour;
+	const char* ampm = (hour < 13 ? "AM" : "PM");
+	hour = hour % 12;
+	if (hour == 0)
+		hour = 12;
+	sprintf(line, "%2d:%02d", hour, gtime->tm_min);
+	OLED->drawString(0, 0, line);
+	OLED->setFont(ArialMT_Plain_10);
+	OLED->drawString(54, 0, ampm);
+	OLED->setFont(ArialMT_Plain_16);
+	sprintf(line, "%d/%d/%2d", gtime->tm_mon + 1, gtime->tm_mday, (gtime->tm_year + 1900) % 100);
+	OLED->drawString(72, 8, line);
+	OLED->setFont(ArialMT_Plain_16);
+	//OLED->drawString(0, 45, daysOfTheWeek[gtime->tm_wday]);
+	float hum = dht.readHumidity();
+	float temp = dht.readTemperature(true);
+	float ctemp = dht.convertFtoC(temp);
+	sprintf(line, "%dF  %0.1fC   %d%%", (int)(temp + 0.5), ctemp, (int)(hum + 0.5));
+	OLED->drawString(0, 45, line);
+#define PSCALE 5
+	for (int x = 0; x <= PSCALE; ++x) {
+		OLED->drawProgressBar(0, 33, 120, 6, x * 100 / PSCALE);
+		OLED->display();
+		delay(1000);
 	}
-	delay(1000);
-	//digitalWrite(17, HIGH);
-	//delay(5);
-	//digitalWrite(17, LOW);
-	//delay(10);
+	thing.handle();
+	//Serial.println(timeClient.getFormattedTime());
+	//digitalWrite(LED, HIGH);   // turn the LED on (HIGH is the voltage level)
+	//delay(2000);              // wait for a second
+	//digitalWrite(LED, LOW);    // turn the LED off by making the voltage LOW
+	//delay(2000);              // wait for a second
+	//float hum = dht.readHumidity();
+	//float temp = dht.readTemperature(true);
+	//if (isnan(hum) || isnan(temp)) {
+	//	Serial.println(F("Failed to read from DHT sensor!"));
+	//	return;
+	//}
+	//Serial.println("Humidity: " + String(hum) + "% " + "Temperature: " + String(temp));
 }
--- __vm/.WiFiClock.vsarduino.h
+++ __vm/.WiFiClock.vsarduino.h
@@ -17,10 +17,8 @@
 #define __ESP32_ESP32__
 #define ESP_PLATFORM
 #define HAVE_CONFIG_H
-#define GCC_NOT_5_2_0 0
-#define WITH_POSIX
 #define F_CPU 240000000L
-#define ARDUINO 108010
+#define ARDUINO 108013
 #define ARDUINO_WIFI_KIT_32
 #define ARDUINO_ARCH_ESP32
 #define ESP32
@@ -94,17 +92,10 @@ typedef long pthread_cond_t;
 #include "arduino.h"
 #include <pins_arduino.h> 
 
-//#include "..\generic\Common.h"
-//#include "..\generic\pins_arduino.h"
-
-//#undef F
-//#define F(string_literal) ((const PROGMEM char *)(string_literal))
-//#undef PSTR
-//#define PSTR(string_literal) ((const PROGMEM char *)(string_literal))
-//current vc++ does not understand this syntax so use older arduino example for intellisense
-//todo:move to the new clang/gcc project types.
 #define interrupts() sei()
 #define noInterrupts() cli()
+
+#define ESP_LOGI(tag, ...)
 
 #include "WiFiClock.ino"
 #endif
